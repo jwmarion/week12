d66532fee193b251c67c03c33b94f81590b91c31
James M <jwmarion@gmail.com>
2017-04-26 19:28:59 -0400

    input program

---
+++ contact/src/index.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactUIDropdown from "react-ui-dropdown";
+import './index.css';
+
+
+class Contact extends React.Component{
+  constructor(){
+    super();
+    this.state=({
+      entry: 1,
+      name: '',
+      phone: '',
+      email: '',
+      type: '',
+      favorite: -1,
+      contacts: [
+        {name: 'james',phone: '3036066666', email: 'jwmarion@yahoo.org', type: 'Sandwich Artist', favorite: 1}]
+    });
+  }
+
+  render(){
+    var fav = ":D"
+    var entry =
+      <div>
+       <div className='input'>
+          <label className='input'>Name</label>
+            <input className='input' type="text" onChange={event=>this.change('name',event)} value={this.state.name}/>
+        </div>
+        <div className='input'>
+          <label className='input' className='input'>Phone</label>
+            <input className='input' type="tel" onChange={event=>this.change('phone',event)} value={this.state.phone}/>
+        </div>
+        <div className='input'>
+          <label className='input'>Email</label>
+          <input className='input' type="email" onChange={event=>this.change('email',event)} value={this.state.email}/>
+        </div>
+        <div className='input'>
+          <div>
+           <ReactUIDropdown
+            label='Type'
+            initialItems={[
+               {id:1,title:'Co-Worker'},
+               {id:2, title:'Family Member'},
+               {id:3, title:'Sandwich Artist'}
+             ]}
+             onChange={event=>this.change('type',event)}/>
+          </div>
+          <button onClick={event=>this.change('contacts',{name:this.state.name, phone: this.state.phone, email: this.state.email, type: this.state.type})}>Submit</button>
+        </div>
+      </div>;
+    return(
+      // let contacts = };
+      //input form
+      <div>
+      <button className="input" onClick={()=>this.setState({entry: this.state.entry * -1})}>Entry Form</button>
+      {this.state.entry === 1?entry:null}
+      {this.state.contacts.map((object,idx)=>
+        <div key={idx} className="user">
+          <h2>{object.name} {object.type}    {object.favorite === 1?fav:'hi'}</h2>
+          <h3>{object.phone}</h3>
+          <h3>{object.email}</h3>
+          <button onClick={event=>this.change('delete',idx)}>Delete</button>
+          <button onClick={event=>this.favChange('favorite',idx)}>Toggle Favorite</button>
+        </div>)}
+      </div>)
+
+  }
+
+  change(stateName,event){
+    var t ='';
+    if(stateName === 'favorite'){
+      t = this.state.favorite;
+      t = t *-1;
+    }
+      if (stateName === 'delete'){
+        t = this.state.contacts
+        t.splice(event,1);
+        console.log(t);
+      }
+      else if(stateName === 'type'){
+        console.log(event[0].title);
+        t = (event[0].title);
+      }
+      else if(stateName === 'contacts'){
+        t = this.state.contacts
+        t.push(event);
+        console.log(t);
+      }
+      else{
+        t = event.target.value;
+        console.log(t);
+      }
+      console.log(t);
+      this.setState({
+        [stateName]: t
+      })
+  }
+  favChange(stateName,index){
+    let t = this.state.contacts;
+    t[index].favorite= t[index].favorite * -1;
+    this.setState({
+      contacts: t
+    })
+
+
+    }
+  }
+
+
+
+
+
+
+ReactDOM.render(<Contact/>,
+  document.getElementById('root')
+);
--- ttt/src/index.js
+++ ttt/src/index.js
@@ -10,55 +10,80 @@ class Board extends React.Component{
     this.state = {
       board: [null,null,null,null,null,null,null,null,null],
       turn: 1,
-      msg: null
+      msg: "React Tac Toe!",
+      endGame: false
+
     }
   }
   render() {
+    let retry = null;
+    if(this.state.endGame){
+      retry = (<div className="retry" onClick={()=>this.newGame(this.state.turn)}>New Game?</div>);
+    }
     return (
+    <div className="game">
+      <div className="top">
+        <h1 className="message">{this.state.msg}</h1>
+      </div>
       <div className="grid">
-      {this.state.board.map((val,i)=>
-       <div className="box" onClick={()=>this.change(i)} key={i}>{this.state.board[i] === -1 ? 'X' : this.state.board[i] === 1 ? 'O' : null}</div>)
-       }
-      <h1 className="message">{this.state.msg}</h1>
-    </div>);
-
+        {this.state.board.map((val,i)=>
+           <div className="box" onClick={()=>this.change(i)} key={i}>{this.state.board[i] === -1 ? 'X' : this.state.board[i] === 1 ? 'O' : null}</div>)}
+           {retry}
+       </div>
+    </div>)
   }
 
   change(val){
     var board = this.state.board;
-    if (this.state.board[val] === null){
-      board[val] = this.state.turn;
-      this.setState({
-        board: board,
-        turn: this.state.turn *-1
-      });
-    }
+    if (this.state.endGame !== true){
+      if (this.state.board[val] === null){
+        board[val] = this.state.turn;
+        this.setState({
+          board: board,
+          turn: this.state.turn *-1
+        });
+      }
+
     //checkWin
     board = this.state.board;
 
-    if(Math.abs(board[0] + board[1] + board[2]) === 3 ||
+    if(
+      Math.abs(board[0] + board[1] + board[2]) === 3 ||
       Math.abs(board[3] + board[4] + board[5]) === 3 ||
       Math.abs(board[6] + board[7] + board[8]) === 3 ||
-      Math.abs(board[0] + board[4] + board[6]) === 3 ||
-      Math.abs(board[1] + board[5] + board[7]) === 3 ||
-      Math.abs(board[2] + board[6] + board[8]) === 3 ||
+      Math.abs(board[0] + board[3] + board[6]) === 3 ||
+      Math.abs(board[1] + board[4] + board[7]) === 3 ||
+      Math.abs(board[2] + board[5] + board[8]) === 3 ||
       Math.abs(board[0] + board[4] + board[8]) === 3 ||
-      Math.abs(board[2] + board[4] + board[6]) === 3){
-        if (this.state.turn == 1 ){
+      Math.abs(board[2] + board[4] + board[6]) === 3 ){
+
+        if (this.state.turn === 1 ){
           var winMsg = 'O Wins!';
         }
         else{
           var winMsg = 'X Wins!';
         }
         this.setState({
-          msg: winMsg
+          msg: winMsg,
+          endGame: true
         });
       }
+      //check draw
+      var end = 0;
+      board.forEach((square)=>{if(square === null){end +=1;}});
 
-
-
-
-
+      if (end === 0){
+        this.setState({msg: "Draw!", endGame: true});
+      }
+    }
+  }
+  newGame(turn){
+    this.setState({
+      board: [null,null,null,null,null,null,null,null,null],
+      turn: turn * -1,
+      msg: "React Tac Toe!",
+      endGame: false
+    });
   }
 
 }
